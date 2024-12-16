2579560e7b611db92de72dbcc96293b905420f6b
Murad <M.Schumacher85@mail.ru>
2024-12-16 22:39:26 +0300

    Подключение библиотек в public

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -4,11 +4,7 @@
     <title>Stilist.pro</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-      @vite([
-               'resources/css/app.css',
-            ])
-
+      @vite(['resources/css/app.css', 'resources/js/app.js'])
   </head>
   <body>
 
@@ -616,21 +612,21 @@
   <!-- loader -->
   <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
 
-  <script src="{{ Vite::asset('resources/js/jquery.min.js') }}"></script>
-  <script src="{{ Vite::asset('resources/js/jquery-migrate-3.0.1.min.js') }}"></script>
-  <script src="{{ Vite::asset('resources/js/popper.min.js') }}"></script>
-  <script src="{{ Vite::asset('resources/js/bootstrap.min.js') }}"></script>
-  <script src="{{ Vite::asset('resources/js/jquery.easing.1.3.js') }}"></script>
-  <script src="{{ Vite::asset('resources/js/jquery.waypoints.min.js') }}"></script>
-  <script src="{{ Vite::asset('resources/js/jquery.stellar.min.js') }}"></script>
-  <script src="{{ Vite::asset('resources/js/owl.carousel.min.js') }}"></script>
-  <script src="{{ Vite::asset('resources/js/jquery.magnific-popup.min.js') }}"></script>
-  <script src="{{ Vite::asset('resources/js/aos.js') }}"></script>
-  <script src="{{ Vite::asset('resources/js/jquery.animateNumber.min.js') }}"></script>
-  <script src="{{ Vite::asset('resources/js/bootstrap-datepicker.js') }}"></script>
-  <script src="{{ Vite::asset('resources/js/jquery.timepicker.min.js') }}"></script>
-  <script src="{{ Vite::asset('resources/js/scrollax.min.js') }}"></script>
-  <script src="{{ Vite::asset('resources/js/main.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('js/jquery.min.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('js/jquery-migrate-3.0.1.min.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('js/popper.min.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('js/bootstrap.min.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('js/jquery.easing.1.3.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('js/jquery.waypoints.min.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('js/jquery.stellar.min.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('js/owl.carousel.min.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('js/jquery.magnific-popup.min.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('js/aos.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('js/jquery.animateNumber.min.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('js/bootstrap-datepicker.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('js/jquery.timepicker.min.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('js/scrollax.min.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('js/main.js') }}"></script>
 
   </body>
 </html>
